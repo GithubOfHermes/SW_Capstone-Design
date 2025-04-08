4ac28d3256db519a8f9dfababde6e7f94d10aa35
KSY <ksy91637@naver.com>
2025-04-08 13:40:34 +0900

    EnemyDamage Version 0.1.0

---
+++ EnemyDamage.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] private float damageAmount = 10f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerController player = collision.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                player.TakeDamage(damageAmount);
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerController player = collision.GetComponent<PlayerController>();
+            if (player != null && !player.IsInvincible())
+            {
+                player.TakeDamage(damageAmount);
+            }
+        }
+    }
+}
